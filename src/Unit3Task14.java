91f09c9c2df50af5e0655f7578a305862e8f5fca
mosh <sergey.moshkin92@gmail.com>
2021-06-05 12:24:04 +0300

    6 задача сделана

--- src/Unit3Task14.java
+++ src/Unit3Task14.java
@@ -3,20 +3,21 @@ import java.util.Random;
 public class Unit3Task14 {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] arr = new int[random.nextInt(10)];
+        int[] arr = new int[9];
 
-        for(int i = 0;i<=arr.length - 1; i++){
-            arr[i] = random.nextInt(20);
+        for(int i = 0; i<arr.length-1; i++){
+            arr[i] = random.nextInt(100);
             System.out.print(arr[i] + " ");
         }
         System.out.println();
         int min = arr[0];
-        for(int i = 0; i<=arr.length - 1;i++){
+        for(int i = 0; i<arr.length-1;i++){
 
-            if(arr[i] < arr[i+1]){
+            if(arr[i] < arr[i+1] && i%2==0){
                 min = arr[i];
             }
-            System.out.println(min);
+
         }
+        System.out.println(min);
     }
 }
--- src/Unit3Task5.java
+++ src/Unit3Task5.java
@@ -1,5 +1,39 @@
+import java.util.Random;
+
 public class Unit3Task5 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+        System.out.print("Первый массив: ");
+        int sum1 = 0;
+        int sum2 = 0;
+        float sr1 = 0;
+        float sr2 = 0;
+        for (int i = 0;i< arr1.length;i++){
+            arr1[i] = random.nextInt(15);
+            System.out.print(arr1[i] + " ");
+            sum1+=arr1[i];
+
+        }
+        System.out.println();
+        System.out.print("Второй массив: ");
+        for (int i = 0;i< arr2.length;i++){
+            arr2[i] = random.nextInt(15);
+            System.out.print(arr2[i] + " ");
+            sum2+=arr2[i];
+
+        }
+        System.out.println();
+        sr1 = sum1/5;
+        sr2 = sum2/5;
+        if(sr1>sr2){
+            System.out.println("Первый массив больше");
+        } else if(sr1<sr2){
+            System.out.println("Второй массив со больше");
+        } else{
+            System.out.println("Массивы равны");
+        }
 
     }
 }
