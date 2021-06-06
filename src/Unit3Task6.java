12bb75d8cd4f9bb6753411ad6c6b59a0df298dce
mosh <sergey.moshkin92@gmail.com>
2021-06-06 11:21:51 +0300

    7 задача сделана

--- src/Unit3Task6.java
+++ src/Unit3Task6.java
@@ -5,13 +5,13 @@ public class Unit3Task6 {
         Random random = new Random();
         int[] arr = new int[4];
         for(int i = 0;i<arr.length;i++){
-            arr[i] = random.nextInt(10);
+           arr[i] = random.nextInt(10);
             System.out.print(arr[i]+" ");
         }
         System.out.println();
         for(int i = 0;i<arr.length;i++){
             if(i>0){
-                if (arr[i-1]>=arr[i]){
+                if (arr[i-1]>arr[i]){
                     System.out.println("Последовательность не возрастает");
                     break;
                 }
---
+++ src/Unit3Task7.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Unit3Task7 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr = new int[12];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(15);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        int max = arr[0];
+        int index=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                index = i;
+            }
+
+
+        }
+        System.out.println("максимальный элемент - "+max);
+        System.out.println("индекс маскимального элемента - "+index);
+    }
+}
